89aa96e936b590e71893474e154ae1dcf67f9c16
GabrielAngelRemus <gabangrem@gmail.com>
2026-04-06 22:10:01 -0300

    trabajojavascript

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+let nombre = "Gabriel";
+let edad = 38;
+let ciudad = "Ciudad Autonoma de Buenos Aires";
+
+console.log("Hola soy", nombre)
+console.log("Mi edad es de", edad)
+console.log("Vivo en", ciudad)
+
+console.log("Hola mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad)
